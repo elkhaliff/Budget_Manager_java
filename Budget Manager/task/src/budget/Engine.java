e08e6330aa950e9386d36614bf413f96711878e5
Old Fox <old.fox@mail.ru>
2022-04-07 14:12:35 +0500

    временный 5/5 есть решение, но тест - не проходит проблема - дублирование ключей меняю местами ключ - значение

--- Budget Manager/task/src/budget/Engine.java
+++ Budget Manager/task/src/budget/Engine.java
@@ -1,10 +1,13 @@
 package budget;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Engine {
 
-    private final Purchases allPurchases;
+    final String SERIAL_FILE_NAME = "purchases.txt";
+
+    private Purchases allPurchases;
 
     public Engine() {
         allPurchases = new Purchases();
@@ -25,6 +28,9 @@ public class Engine {
                         "2) Add purchase\n" +
                         "3) Show list of purchases\n" +
                         "4) Balance\n" +
+                        "5) Save\n" +
+                        "6) Load\n" +
+                        "7) Analyze (Sort)\n" +
                         "0) Exit";
 
         final String MENU_LVL2 =
@@ -44,17 +50,42 @@ public class Engine {
                         "5) All\n" +
                         "6) Back";
 
+        final String MENU_LVL4 =
+                "\nHow do you want to sort?\n" +
+                        "1) Sort all purchases\n" +
+                        "2) Sort by type\n" +
+                        "3) Sort certain type\n" +
+                        "4) Back";
+
+
+        final String MENU_LVL5 =
+                "\nChoose the type of purchase\n" +
+                        "1) Food\n" +
+                        "2) Clothes\n" +
+                        "3) Entertainment\n" +
+                        "4) Other";
+
         final int LVL_1 = 1;
         final int LVL_2 = 2;
         final int LVL_3 = 3;
+        final int LVL_4 = 4;
+        final int LVL_5 = 5;
 
         final int ADD_INCOME = 1;
         final int ADD_PURCHASE = 2;
         final int SHOW_LIST = 3;
         final int BALANCE = 4;
+        final int SAVE = 5;
+        final int LOAD = 6;
+        final int SORT = 7;
+
+        final int SORT_ALL = 1;
+        final int SORT_BY_TYPE = 2;
+
         final int EXIT = 0;
         final int BACK2 = 5;
         final int BACK3 = 6;
+        final int BACK4 = 4;
 
         int currLvl = LVL_1;
         int currAction;
@@ -62,7 +93,9 @@ public class Engine {
             String menu;
             if (currLvl == LVL_1) menu = MENU_LVL1;
             else if (currLvl == LVL_2) menu = MENU_LVL2;
-            else menu = MENU_LVL3;
+            else if (currLvl == LVL_3) menu = MENU_LVL3;
+            else if (currLvl == LVL_4) menu = MENU_LVL4;
+            else menu = MENU_LVL5;
 
             currAction = getInt(menu);
 
@@ -71,6 +104,24 @@ public class Engine {
                 else if (currAction == ADD_PURCHASE)  currLvl = LVL_2;
                 else if (currAction == SHOW_LIST)  currLvl = LVL_3;
                 else if (currAction == BALANCE)  allPurchases.getBalance();
+                else if (currAction == SAVE)  {
+                    try {
+                        SerializationUtils.serialize(allPurchases, SERIAL_FILE_NAME);
+                        println("\nPurchases were saved!");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+                else if (currAction == LOAD)  {
+                    try {
+                        allPurchases = (Purchases) SerializationUtils.deserialize(SERIAL_FILE_NAME);
+                        println("\nPurchases were loaded!");
+                    } catch (IOException | ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else if (currAction == SORT)  currLvl = LVL_4;
                 else if (currAction == EXIT) {
                     println("\nBye!");
                     System.exit(0);
@@ -81,10 +132,21 @@ public class Engine {
             } else if (currLvl == LVL_3) {
                 if (currAction < BACK3)  allPurchases.showPurchase(currAction);
                 else if (currAction == BACK3) currLvl = LVL_1;
+            } else if (currLvl == LVL_4) {
+                if (currAction < BACK4) {
+                    if (currAction == SORT_ALL || currAction == SORT_BY_TYPE)
+                        allPurchases.sortPurchaseAll(currAction == SORT_BY_TYPE);
+                    else {
+                        currLvl = LVL_5;
+                    }
+                }
+                else if (currAction == BACK4) currLvl = LVL_1;
+            } else if (currLvl == LVL_5) { // choose type
+                allPurchases.sortPurchaseByCertainType(currAction);
+                currLvl = LVL_4;
             } else {
                 throw new IllegalStateException("Unexpected action: " + currAction);
             }
         }
     }
-
 }
--- Budget Manager/task/src/budget/Purchases.java
+++ Budget Manager/task/src/budget/Purchases.java
@@ -1,25 +1,26 @@
 package budget;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.io.Serializable;
+import java.util.*;
 
-public class Purchases {
+public class Purchases implements Serializable {
     private Double balance;
     Double total;
+    boolean isEmpty;
     double amount;
 
     private final int ALL = 5;
+
     private final String[] types = {"", "Food", "Clothes", "Entertainment", "Other", "All"};
 
     private final Map<Integer, Map<String, Double>> purchases;
 
     public Purchases() {
         balance = 0D;
-        purchases = new HashMap<>();
+        purchases = new LinkedHashMap<>();
 
         for (int i = 1; i < ALL; i++) {
-            purchases.put(i, new HashMap<>());
+            purchases.put(i, new LinkedHashMap<>());
         }
     }
 
@@ -48,7 +49,7 @@ public class Purchases {
     }
 
     public void getBalance() {
-        println(String.format("\nBalance: $%s\n", balance.toString()));
+        println(String.format("\nBalance: $%.2f", balance));
     }
 
     public void addPurchase(int type) {
@@ -63,23 +64,109 @@ public class Purchases {
         total = 0D;
 
         println(String.format("\n%s:", types[type]));
-        if (type < ALL) {
-            total = showList(type, purchases.get(type));
+        if (type == ALL) {
+            purchases.forEach((tp, purchase) -> total += showMap(purchase));
         } else
-            purchases.forEach((tp, purchase) -> { total += showList(tp, purchase);});
-        println(String.format("Total sum: $%s", total.toString()));
+            total = showMap(purchases.get(type));
+        println(String.format("Total sum: $%.2f", total));
     }
 
-    private double showList(int type, Map purchase) {
+    private double showMap(Map<String, Double> map) {
         amount = 0D;
-        if (purchase.isEmpty())
+        if (map.isEmpty())
             println("The purchase list is empty");
         else
-            purchase.forEach((purch, price) -> {
-                println(String.format("%s %s", purch, price.toString()));
-                amount += (double)price;
+            map.forEach((purchase, price) -> {
+                println(String.format("%s $%.2f", purchase, price));
+                amount += price;
             });
         return amount;
     }
 
+    public void sortPurchaseAll(boolean isSortByType) {
+        total = 0D;
+        int type = ALL;
+        Map<String, Double> sortedMap;
+
+        if (!isSortByType && isEmpty(type))
+            println("\nThe purchase list is empty!");
+        else{
+            if (isSortByType) {
+                println("\nTypes:");
+                sortedMap = getSummaryMapByType(type);
+            } else {
+                println(String.format("\n%s:", types[type]));
+                sortedMap = getSortedMapByType(type);
+            }
+            total = showMap(sortedMap);
+            println(String.format("Total sum: $%.2f", total));
+        }
+    }
+
+    private boolean isEmpty(int type) {
+        isEmpty = false;
+
+        if (type == ALL) {
+            purchases.forEach((tp, purchase) -> isEmpty = isEmpty || purchase.isEmpty());
+        } else
+            isEmpty = purchases.get(type).isEmpty();
+
+        return isEmpty;
+    }
+
+    public void sortPurchaseByCertainType(int type) {
+        total = 0D;
+        Map<String, Double> sortedMap;
+
+        if (isEmpty(type))
+            println("\nThe purchase list is empty!");
+        else {
+            println("\n%s:, types[type]");
+            sortedMap = getSortedMapByType(type);
+
+            total = showMap(sortedMap);
+            println(String.format("Total sum: $%.2f", total));
+        }
+    }
+
+    private Map<String, Double> getSortedMapByType(int type) {
+        Map<String, Double> sortedMap = new TreeMap<>();
+        if (type == ALL) {
+            purchases.forEach((tp, purchase) -> sortedMap.putAll(purchase));
+        } else
+            sortedMap.putAll(purchases.get(type));
+        return sortByValues(sortedMap);
+    }
+
+    private Map<String, Double> getSummaryMapByType(int type) {
+        Map<String, Double> sortedMap = new TreeMap<>();
+        if (type == ALL) {
+            purchases.forEach((tp, purchase) -> sortedMap.putAll(getSummaByType(tp, purchase)));
+        } else
+            sortedMap.putAll(getSummaByType(type, purchases.get(type)));
+        return sortByValues(sortedMap);
+    }
+
+    private Map<String, Double> getSummaByType(int type, Map<String, Double> map) {
+        Map<String, Double> retMap = new HashMap<>();
+        amount = 0D;
+        if (!map.isEmpty())
+            map.forEach((purchase, price) -> amount += price);
+        retMap.put(types[type], amount);
+        return retMap;
+    }
+
+    private  <K, V extends Comparable<V>> Map<K, V> sortByValues(final Map<K, V> map) {
+        Comparator<K> valueComparator = (k1, k2) -> {
+            int compare =
+                    map.get(k1).compareTo(map.get(k2));
+            if (compare == 0)
+                return 1;
+            else
+                return -compare;
+        };
+        Map<K, V> sortedByValues = new TreeMap<K, V>(valueComparator);
+        sortedByValues.putAll(map);
+        return sortedByValues;
+    }
 }
